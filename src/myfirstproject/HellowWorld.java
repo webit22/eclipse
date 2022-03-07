21e75bdb2c97fe6630070f050fd016a22fc44ada
jbyun <binifia@gmail.com>
2022-03-07 14:08:30 +0900

    test

--- src/myfirstproject/HellowWorld.java
+++ src/myfirstproject/HellowWorld.java
@@ -4,13 +4,5 @@ public class HellowWorld {
 
 	public static void main(String[] args) {
 		System.out.println("hellow");
-		System.out.println("hellow");
-		System.out.println("hellow");
-		System.out.println("how");
-		System.out.println("howwww");
-		System.out.println("h");
-		System.out.println("g");
-		System.out.println("o");
-		System.out.println("hi");
 	}
 }
