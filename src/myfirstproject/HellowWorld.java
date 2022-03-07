508dd62aa4163fcce6f1c19f40ab7c3ae8244163
JYP <pjy2958@naver.com>
2022-03-07 13:22:22 +0900

    12

--- src/myfirstproject/HellowWorld.java
+++ src/myfirstproject/HellowWorld.java
@@ -1,10 +1,12 @@
 package myfirstproject;
 
 public class HellowWorld {
+
 	public static void main(String[] args) {
 		System.out.println("hellow");
 		System.out.println("hellow");
 		System.out.println("hellow");
 		System.out.println("how");
+		System.out.println("howwww");
 	}
 }
